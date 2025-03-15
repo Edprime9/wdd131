4e41bfeaba17f333f63f023b20cb33842b240238
Edprimw9 <eli24007@byui.edu>
2025-03-15 22:10:10 +0100

    bom

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,11 +2,35 @@ const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("ul");
 
-const li = document.createElement("li");
-const deleteButton = document.createElement("button");
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') { 
+        const li = document.createElement('li');
+        li.textContent = input.value;
 
-li.textContent = input.value;
-deleteButton.textContent = "X";
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "X";
+        deleteButton.classList.add('delete-btn');
 
-li.append(deleteButton);
-list.append(li);
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+
+        deleteButton.addEventListener('click', function () {
+        list.removeChild(li);
+        input.focus();
+
+        });
+
+        input.value = '';
+     }
+
+     input.focus();
+
+  });
+
+
+
+
+
+
+
+ 
